b9364bab04ec644a2a23ae7418c25a66c0f242fc
wagih <awagih851@gmail.com>
2025-08-20 16:18:59 +0300

    fix vite base for vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/prayer-time/', 
-  plugins: [react()]
+  base: './',
+  plugins: [react()],
 });
